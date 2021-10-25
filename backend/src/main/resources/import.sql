f9884be32c5ea01b6beb0055a0772566afc452fc
Adroaldo <hygino@msn.com>
2021-10-25 10:34:21 -0300

    Criado o método findAll e exceção personalizada

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO employees(first_name,last_name,email_id) values ('Ichigo','Kurosaki','ichigo@soulsociety.com');
+INSERT INTO employees(first_name,last_name,email_id) values ('Rukia','Kuchiki','rukia@soulsociety.com');
+INSERT INTO employees(first_name,last_name,email_id) values ('Byakuya','kuchiki','byakuya@soulsociety.com');
+INSERT INTO employees(first_name,last_name,email_id) values ('Zarachi','Kempachi','zarachi@soulsociety.com');
+INSERT INTO employees(first_name,last_name,email_id) values ('Ikaku','Madarame','ikaku@soulsociety.com');
+INSERT INTO employees(first_name,last_name,email_id) values ('Yumichika','Ayasegawa','yumichica@soulsociety.com');
